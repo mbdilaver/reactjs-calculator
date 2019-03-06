a3c72dce143350edf9b40d6b356e09f49b17fb36
mbdilaver <mbdilaver@gmail.com>
2019-03-06 08:40:07 +0300

    Updates

---
+++ precache-manifest.fe01fbf2cc87147962888ecd96b3a968.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8fc3fd54e801a253be1",
+    "url": "/reactjs-calculator/static/js/runtime~main.c8fc3fd5.js"
+  },
+  {
+    "revision": "c1c4326926d5fd626a59",
+    "url": "/reactjs-calculator/static/js/main.c1c43269.chunk.js"
+  },
+  {
+    "revision": "c85f065afdff2fcc37a0",
+    "url": "/reactjs-calculator/static/js/2.c85f065a.chunk.js"
+  },
+  {
+    "revision": "c1c4326926d5fd626a59",
+    "url": "/reactjs-calculator/static/css/main.a8827278.chunk.css"
+  },
+  {
+    "revision": "bfb8f52805d9abe703aa40326755e6f2",
+    "url": "/reactjs-calculator/index.html"
+  }
+];
